5e173be92e5206d6405f65158fd8312d510f13d9
parkhojeong <parkhj062@gmail.com>
2020-12-25 19:44:00 +0900

    first commit conversion app

---
+++ conversionApp/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.shortcuts import render
+import googleAPI
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'index.html', {'msg':'need to upload image'})
+
+    elif request.method == "POST":
+        res = googleAPI.orc_kor_eng(request.FILES['image'].file)
+        return render(request,'index.html',{'msg': res})

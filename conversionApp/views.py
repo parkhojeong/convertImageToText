379dcb2ad0e272e8afc222ddc9ca1b7185b89424
koreahong <koreahong2@naver.com>
2020-12-30 12:31:16 +0900

    sort_text_final

--- conversionApp/views.py
+++ conversionApp/views.py
@@ -9,10 +9,10 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def index(request):
     if request.method == 'GET':
-        return render(request, 'index.html', {'msg':'need to upload image'})
+        return render(request, 'index.html', {'start':'need to upload image'})
 
     elif request.method == "POST":
         res = googleAPI.orc_kor_eng(request.FILES['image'].file)
         res = res.split('\n')
-        res = res[:len(res) // 2]
+        res = res[:len(res) // 2 + 1]
         return render(request,'index.html',{'msg': res})
